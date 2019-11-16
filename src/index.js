5b509ee8289f86628fe330edcad671e7eb2aab0a
vinicius <viniciusescuderoinstituto@gmail.com>
2019-11-16 14:37:24 +0100

    send connect request to tracker

--- src/index.js
+++ src/index.js
@@ -2,10 +2,49 @@
 
 const fs = require('fs');
 const bencode = require('bencode');
+const dgram = require('dgram');
+const Buffer = require('buffer').Buffer;
+const url = require('url');
+const crypto = require('crypto');
 
-const torret_file_name = 'test.torrent';
-const torrent_file = fs.readFileSync(torret_file_name);
+function buildConnectRequest() {
+    // See: http://www.bittorrent.org/beps/bep_0015.html
+    const buffer = Buffer.allocUnsafe(16);
+    const magic_h = 0x417;
+    const magic_l = 0x27101980;
+    const connectAction = 0x0;
+    const transactionId = crypto.randomBytes(4);
 
-const parsed_torrent = bencode.decode(torrent_file, 'utf-8');
+    buffer.writeUInt32BE(magic_h, 0);
+    buffer.writeUInt32BE(magic_l, 4);
+    buffer.writeUInt32BE(connectAction, 8);
+    transactionId.forEach((value, i, buf) => {
+        buffer.writeUInt8(value, 12 + i);
+    })
 
-console.log(parsed_torrent);
+    // We will need the transaciton id later
+    return [transactionId, buffer]; 
+}
+
+const torretFileName = 'test.torrent';
+const torrentFile = fs.readFileSync(torretFileName);
+
+const parsedTorrent = bencode.decode(torrentFile);
+const announceURL = url.parse(parsedTorrent.announce.toString('utf-8'));
+console.log(announceURL);
+
+const socket = dgram.createSocket('udp4');
+
+socket.on('error', (err) => {
+    console.log(`socket error:\n${err.stack}`);
+    socket.close();
+})
+
+socket.on('message', (msg) => {
+    console.log('message received ', msg);
+});
+
+const [transactionId, payload] = buildConnectRequest();
+console.log(transactionId);
+console.log(payload);
+socket.send(payload, announceURL.port, announceURL.hostname);
